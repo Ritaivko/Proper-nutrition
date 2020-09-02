52cb6efbeefff2a9f68ecf888bad3f70b3d63a99
Ritaivko <rita26111@gmail.com>
2020-09-02 14:34:13 +0300

    new pass

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
-let alcoholic = prompt('Вы алкаш?', '');
+/*let alcoholic = prompt('Вы алкаш?', '');
 if (alcoholic == 'Да') alert( 'Вы правы!' );
+
+outer: 
+    for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        let input = prompt(`Значение на координатах (${i},${j})`, '');
+
+        if (!input) break outer;
+    }
+}
+alert('Готово!');
+*/
+
+let userName = prompt(`Кто там?`, '');
+
+if(userName == 'Алкаш') {
+    
+    let pass = prompt(`Пароль?`, '');
+    if( pass == 'Шампанское') {
+        alert('Здравствуйте!');
+    } else if( pass == '' || pass == null) {
+        alert ('Отменено');
+    } else {
+        alert('Неверный пароль');
+    }
+
+    } else if(userName == '' || userName == null) {
+        alert('Отменено');
+    } else {
+        alert('Я вас не знаю');
+    }
+
+
+
